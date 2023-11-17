0c39461054f5cf36ff21f03899ab35e564f9c74b
jacob70293 <jacob70293@gmail.com>
2023-11-18 00:07:17 +0800

    refactor(1930-unique-length-3-palindromic-subsequences): optimize runtime and memory

--- 20-valid-parentheses/main.go
+++ 20-valid-parentheses/main.go
@@ -1,42 +1,51 @@
 package _20_valid_parentheses
 
-var leftParentheses = map[string]bool{
-	"(": true,
-	"[": true,
-	"{": true,
-}
+import (
+	"slices"
+)
 
-var pairParentheses = map[string]string{
-	")": "(",
-	"]": "[",
-	"}": "{",
+const (
+	LeftParenthesis  rune = '('
+	RightParenthesis rune = ')'
+	LeftBracket      rune = '['
+	RightBracket     rune = ']'
+	LeftBrace        rune = '{'
+	RightBrace       rune = '}'
+)
+
+var pairParentheses map[rune]rune
+
+func init() {
+	pairParentheses = map[rune]rune{
+		LeftParenthesis: RightParenthesis,
+		LeftBracket:     RightBracket,
+		LeftBrace:       RightBrace,
+	}
 }
 
 func isValid(s string) bool {
-	var tempStack []string
+	var runeStack []rune
 
 	for _, r := range s {
 		// 符合左括號，放進暫存列表
-		if leftParentheses[string(r)] {
-			tempStack = append(tempStack, string(r))
-		} else { // 右括號
-			if len(tempStack) == 0 {
-				return false
-			}
-
-			// pop stack 比對
-			popEle := tempStack[len(tempStack)-1]
-			if pairParentheses[string(r)] != popEle {
-				return false
-			}
-
-			tempStack = tempStack[0 : len(tempStack)-1]
+		if slices.Contains([]rune{LeftParenthesis, LeftBracket, LeftBrace}, r) {
+			runeStack = append(runeStack, r)
+			continue
 		}
+
+		if len(runeStack) == 0 {
+			return false
+		}
+
+		leftRune := runeStack[len(runeStack)-1]
+
+		// pop stack 比對
+		if pairParentheses[leftRune] != r {
+			return false
+		}
+
+		runeStack = runeStack[0 : len(runeStack)-1]
 	}
 
-	if len(tempStack) > 0 {
-		return false
-	}
-
-	return true
+	return len(runeStack) == 0
 }
